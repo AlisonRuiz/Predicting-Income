11e9f0facdbc2dffad801b2bf46f4a63cdb19eb9
josejparra <julianparra2103@gmail.com>
2022-09-04 23:25:10 -0500

    actualizacion scripts

--- scripts/web_scrapping.R
+++ scripts/web_scrapping.R
@@ -17,22 +17,29 @@ urls_tables_page <- read_html(url_home) %>%
   html_attr("href") %>% 
   grep("page",.,value=T)
 
-#Inicializar tibble vacio.
-geih <- tibble()
 
-#Iterar sobre los enlaces que nos da la página de Ignacio.
+urls_tablas <- list()
+#Iterar sobre los enlaces que nos da la página de Ignacioy extraer los enlaces de las tablas.
 for (i in 1:length(urls_tables_page)){
   
   #Url de la página donde está embebida la tabla
   url_table_page <- paste0(url_home,urls_tables_page[[i]])
   #se extrae parte de la Url de la ubicación de la tabla
   html_table_page <- read_html(url_table_page)
-  table_url <- html_table_page %>% 
+  urls_tablas[i] <- html_table_page %>% 
     html_elements("div") %>% 
     html_attr("w3-include-html") %>% 
     grep("page",.,value=T)
+  
+  
+}
+
+#Inicializar tibble vacio.
+geih <- tibble()
+#Leer las tablas embebidas en las páginas a las que lleva la página principal
+for (i in 1:length(urls_tablas)){
   #url de la tabla
-  url_table <- paste0(url_home,table_url)
+  url_table <- paste0(url_home,urls_tablas[[i]])
   html_tabla <- read_html(url_table)
   #tomar trozo de la tabla
   table_part <- html_tabla %>% html_table()
@@ -41,7 +48,4 @@ for (i in 1:length(urls_tables_page)){
   
   print(i)
 }
-
-#Existe una advertencia en algunos momentos en los que se corre la funcion, pero la tabla de datos
-#que retorna es correcta.
 
